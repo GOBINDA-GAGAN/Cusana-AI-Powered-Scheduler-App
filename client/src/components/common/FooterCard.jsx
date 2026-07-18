935753cd7322e9ab761f9f044d967eb8b9e552bc
GOBINDA-GAGAN <gobindagagandey@gmail.com>
2026-07-18 20:36:34 +0530

    #

--- client/src/components/common/FooterCard.jsx
+++ client/src/components/common/FooterCard.jsx
@@ -3,20 +3,7 @@ import React from "react";
 const FooterCard = ({ title, dec }) => {
   return (
     <div
-      className="
-        w-full
-        rounded-xl
-        bg-linear-to-b
-        from-[#2F2F2F]
-        to-[#1F1F1F]
-        px-4
-        py-5
-        text-center
-        shadow-[inset_0_1px_0_rgba(255,255,255,0.35),0_12px_25px_rgba(0,0,0,0.45)]
-        transition-all
-        duration-300
-        hover:-translate-y-1
-        hover:shadow-[inset_0_1px_0_rgba(255,255,255,0.35),0_20px_40px_rgba(0,0,0,0.55)]
+      className=" w-full rounded-xl bg-linear-to-b from-[#2F2F2F] to-[#1F1F1F] px-4 py-5 text-center shadow-[inset_0_1px_0_rgba(255,255,255,0.35),0_12px_25px_rgba(0,0,0,0.45)] transition-all duration-300 hover:-translate-y-1 hover:shadow-[inset_0_1px_0_rgba(255,255,255,0.35),0_20px_40px_rgba(0,0,0,0.55)]
       "
     >
       <h3 className="text-2xl font-bold tracking-tight text-white sm:text-3xl">
